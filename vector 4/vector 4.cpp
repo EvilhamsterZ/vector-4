ab54681d4ea2f1b502217f4d061a537824b98249
Sergei <90544365+EvilhamsterZ@users.noreply.github.com>
2021-12-23 21:06:54 +0300

    Add files via upload

---
+++ vector 4/vector 4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{   
+    int n;
+    std::vector<int> v;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back(i);
+    }
+    std::cout<< sizeof(std::vector<int>) + (sizeof(int) * v.size());
+}
